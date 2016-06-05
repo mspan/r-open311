3cc6ffb5ae57aa3da5d1f74480279953351558f3
mspan <mspaner@gmail.com>
2016-06-05 19:50:09 -0400

    generally working. Need to move some functions over to the helper file

--- SCFhelper.R
+++ SCFhelper.R
@@ -1,25 +1,42 @@
 #SCF helper functions
-get_page_of_issues <- function(url,page_num,per_page)
-{
-  final_url = paste0(url,page_num,"&per_page=", per_page)
-  print (final_url)
-  issue_list = fromJSON(final_url, flatten =TRUE)
-  return (issue_list)
-}
-
 get_multiple_pages_of_issues<- function(url,page_num,per_page,number_of_pages) 
 {
-  
+  df=data.frame()
   for (i in 1:number_of_pages) 
   {
-    print("page number")
-    print(i)
-    print(" number of pages to fetch")
-    print(number_of_pages)
+    #print("page number")
+    #print(i)
+    #print(" number of pages to fetch")
+    #print(number_of_pages)
     res = get_page_of_issues(url,i,per_page)
     rissues = res$issues
     df = rbind(df,rissues)
     
   }
+  
   return(df)
 }
+
+get_page_of_issues <- function(url,page_num,per_page)
+{
+  final_url = paste0(url,page_num,"&per_page=", per_page)
+  #print (final_url)
+  issue_list = fromJSON(final_url, flatten =TRUE)
+  return (issue_list)
+}
+
+plot_scf_issues<- function(dsp)
+{
+  p = ggplot(dsp, aes(created_at, acknowledged_at)) + 
+    theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=4)) +
+    geom_point(aes(color = status), alpha = 0.7) 
+  (gg <- ggplotly(p))
+}
+
+get_open_311 = function(url)
+{
+  print (url)
+  res = fromJSON(url, flatten =TRUE)
+  return(res)
+  }
+
--- getSCFdata.R
+++ getSCFdata.R
@@ -3,68 +3,74 @@ library(dplyr)
 library(ggplot2)
 library(lubridate) # for working withe dates and times
 
-#remove all variables and values the environment
 rm(list=ls(all=TRUE)) 
-
-max_pages=1000 # be respectful, limit the number of pages being polled.
-df=data.frame() #stores the consolidated list of issues from SeeClickFix
-for (i in 1:max_pages) 
-{
-  #construct the URL to retrieve a page of data
-  url = paste0("http://test.seeclicktest.com/api/issues.json?at=Burlington,+VT&start=50000&end=0&page=", toString(i), "&num_results=100&sort=issues.created_at")
-  # print(url)
-  seeclick_data <- fromJSON(paste(readLines(url), collapse=""))
-  df1 = ldply (seeclick_data, data.frame, stringsAsFactors = FALSE )
-  
-  if ( length(df1)== 0 ) {  #if no more data is available, an empy record is returned. 
-    breakFlag = TRUE
-    break
-  }
-  df = rbind(df,df1)        # append the page of data to the overall results. 
-  
-}
-page_num = 1
-#page_num =3367
-per_page = 100
-url = paste0("https://www.seeclickfix.com/api/v2/issues?page=",page_num,"&per_page=", per_page = 100)
-data1 <- fromJSON(url, flatten =TRUE)
-names(data1)
-scf_issues = data.frame(data1$issues)  
-scf_meta_list = data1$metadata
-scf_meta_list$pagination$entries
-scf_meta_list$pagination$page
-scf_meta_list$pagination$per_page
-scf_meta_list$pagination$pages
-scf_meta_list$pagination$next_page
-scf_meta_list$pagination$next_page_url
-scf_meta_list$pagination$previous_page
-scf_meta_list$pagination$previous_page_url
-scf_error_list = data1$errors
-
 source("SCFhelper.R")
-df=data.frame()
+library(plotly)
+#install.packages(c("shiny", "htmlwidgets"))
 
 
 
-for (i in 1:3)
-{
-  print(i)
-}
 
+#remove all variables and values the environment
+
+# 
+# max_pages=1000 # be respectful, limit the number of pages being polled.
+# df=data.frame() #stores the consolidated list of issues from 
+# page_num = 1
+# 
+# per_page = 500
+# url = paste0("https://www.seeclickfix.com/api/v2/issues?page=",page_num,"&per_page=", per_page = 500)
+# data1 <- fromJSON(url, flatten =TRUE)
+# dataa1_open_issues= data1$issues %>% filter(status=="Open")
+# names(data1)
+# 
+# scf_issues = data1$issues  
+# scf_meta_list = data1$metadata
+# scf_meta_list$pagination$entries
+# scf_meta_list$pagination$page
+# scf_meta_list$pagination$per_page
+# scf_meta_list$pagination$pages
+# scf_meta_list$pagination$next_page
+# scf_meta_list$pagination$next_page_url
+# scf_meta_list$pagination$previous_page
+# scf_meta_list$pagination$previous_page_url
+# scf_error_list = data1$errors
 
 url_base = "https://www.seeclickfix.com/api/v2/issues?page="
-page_num = 1
-per_page = 1000
-# d1 = get_page_of_issues(url_base,page_num,per_page )
-# nrow(d1$issues)
-# page_num = 2
-# d2 =get_page_of_issues(url_base,page_num,per_page )
-# nrow(d2$issues)
-# d3_issues <- rbind(d1$issues,d2$issues)
-# nrow(d3_issues)
-number_of_pages = 3
-dx =get_multiple_pages_of_issues(url_base,page_num,per_page,number_of_pages)
-d4 =get_page_of_issues(url_base,3,per_page )
-d4d = data.frame(d4$issues)
+url311discover = "https://www.seeclickfix.com/open311/v2/29/discovery.json"
+url311req = "https://www.seeclickfix.com/open311/v2/29/requests.json"
+page_num = 2
+per_page = 80
 
 
+
+
+d_single_page =get_page_of_issues(url_base,page_num,per_page )
+dsp = data.frame(d_single_page$issues)
+plot_scf_issues(dsp)
+
+number_of_pages = 2
+
+dmultp =get_multiple_pages_of_issues(url_base,page_num,per_page,number_of_pages)
+plot_scf_issues(dmultp)
+
+dmultp$new_created_at =  ymd_hms(dmultp$created_at)
+summary(dmultp$created_at)
+summary(dmultp$new_created_at)
+str(dmultp$new_created_at)
+
+p = ggplot(dmultp, aes(new_created_at, acknowledged_at)) + 
+    scale_x_datetime(date_breaks = "1 min")+
+  theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=8)) +
+  geom_point(aes(color = status), alpha = 0.7) 
+(gg <- ggplotly(p))
+
+
+
+x = head(dmultp$created_at,50)
+x
+length(x)
+
+#311
+resreq = get_open_311(url311req )
+resdisc = get_open_311(url311discover  )
