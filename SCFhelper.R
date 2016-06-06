88c60925242896e03333544efc23e1f58b03c606
mspan <mspaner@gmail.com>
2016-06-05 21:26:22 -0400

    replaced rbind with (dplyr) bind_rows to account for transistions.urlopen not always being present in the response

--- SCFhelper.R
+++ SCFhelper.R
@@ -1,23 +1,9 @@
 #SCF helper functions
-get_multiple_pages_of_issues<- function(url,page_num,per_page,number_of_pages) 
-{
-  df=data.frame()
-  for (i in 1:number_of_pages) 
-  {
-    #print("page number")
-    #print(i)
-    #print(" number of pages to fetch")
-    #print(number_of_pages)
-    res = get_page_of_issues(url,i,per_page)
-    rissues = res$issues
-    df = rbind(df,rissues)
-    
-  }
-  
-  return(df)
-}
-
-get_page_of_issues <- function(url,page_num,per_page)
+library(jsonlite)
+library(dplyr)
+library(ggplot2)
+library(lubridate) # for working withe dates and times
+get_page_of_results <- function(url,page_num,per_page)
 {
   final_url = paste0(url,page_num,"&per_page=", per_page)
   #print (final_url)
@@ -25,10 +11,32 @@ get_page_of_issues <- function(url,page_num,per_page)
   return (issue_list)
 }
 
+get_multiple_pages_of_issues<- function(url,page_num,per_page,number_of_pages) 
+{
+  df=data.frame()
+  for (i in 1:number_of_pages) 
+  {
+    res = get_page_of_results(url,i,per_page)
+    rissues = res$issues
+    # print("ncol")
+    # print(names(rissues))
+    # ncol(rissues)
+    df = bind_rows(df,rissues)
+  }
+  
+  return(df)
+}
+
+
+
 plot_scf_issues<- function(dsp)
 {
-  p = ggplot(dsp, aes(created_at, acknowledged_at)) + 
-    theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=4)) +
+  dsp$new_created_at =  ymd_hms(dsp$created_at)
+  dsp$new_acknowledged_at =  ymd_hms(dsp$acknowledged_at)
+  p = ggplot(dsp, aes(new_created_at, new_acknowledged_at)) + 
+    scale_x_datetime(date_breaks = "5 min")+
+    scale_y_datetime(date_breaks = "5 min")+
+    theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=8)) +
     geom_point(aes(color = status), alpha = 0.7) 
   (gg <- ggplotly(p))
 }
--- getSCFdata.R
+++ getSCFdata.R
@@ -1,75 +1,29 @@
-library(jsonlite)
-library(dplyr)
-library(ggplot2)
-library(lubridate) # for working withe dates and times
+#rm(list=ls(all=TRUE)) 
 
-rm(list=ls(all=TRUE)) 
+
+
 source("SCFhelper.R")
 library(plotly)
-#install.packages(c("shiny", "htmlwidgets"))
 
-
-
-
-#remove all variables and values the environment
-
-# 
-# max_pages=1000 # be respectful, limit the number of pages being polled.
-# df=data.frame() #stores the consolidated list of issues from 
-# page_num = 1
-# 
-# per_page = 500
-# url = paste0("https://www.seeclickfix.com/api/v2/issues?page=",page_num,"&per_page=", per_page = 500)
-# data1 <- fromJSON(url, flatten =TRUE)
-# dataa1_open_issues= data1$issues %>% filter(status=="Open")
-# names(data1)
-# 
-# scf_issues = data1$issues  
-# scf_meta_list = data1$metadata
-# scf_meta_list$pagination$entries
-# scf_meta_list$pagination$page
-# scf_meta_list$pagination$per_page
-# scf_meta_list$pagination$pages
-# scf_meta_list$pagination$next_page
-# scf_meta_list$pagination$next_page_url
-# scf_meta_list$pagination$previous_page
-# scf_meta_list$pagination$previous_page_url
-# scf_error_list = data1$errors
 
 url_base = "https://www.seeclickfix.com/api/v2/issues?page="
 url311discover = "https://www.seeclickfix.com/open311/v2/29/discovery.json"
 url311req = "https://www.seeclickfix.com/open311/v2/29/requests.json"
-page_num = 2
-per_page = 80
+page_num = 1
+per_page = 200
+number_of_pages = 3
 
 
 
+d_single_page =get_page_of_results(url_base,page_num,per_page )
 
-d_single_page =get_page_of_issues(url_base,page_num,per_page )
-dsp = data.frame(d_single_page$issues)
-plot_scf_issues(dsp)
+plot_scf_issues(d_single_page$issues)
 
-number_of_pages = 2
+
 
 dmultp =get_multiple_pages_of_issues(url_base,page_num,per_page,number_of_pages)
+
 plot_scf_issues(dmultp)
-
-dmultp$new_created_at =  ymd_hms(dmultp$created_at)
-summary(dmultp$created_at)
-summary(dmultp$new_created_at)
-str(dmultp$new_created_at)
-
-p = ggplot(dmultp, aes(new_created_at, acknowledged_at)) + 
-    scale_x_datetime(date_breaks = "1 min")+
-  theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=8)) +
-  geom_point(aes(color = status), alpha = 0.7) 
-(gg <- ggplotly(p))
-
-
-
-x = head(dmultp$created_at,50)
-x
-length(x)
 
 #311
 resreq = get_open_311(url311req )
